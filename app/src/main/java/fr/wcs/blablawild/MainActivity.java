50a0c26a6b78e5e7d050b9a55eca3bc96364cd4c
Nono <lombardo.nora@gmail.com>
2017-10-02 12:24:11 +0200

    traduction

--- app/src/main/java/fr/wcs/blablawild/MainActivity.java
+++ app/src/main/java/fr/wcs/blablawild/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button1 = (Button)findViewById(R.id.button1);
-
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
